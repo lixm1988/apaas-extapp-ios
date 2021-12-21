49caf56be23b38fb47d9323b6433033b3f235642
刘元媛 <liuyuanyuan02@agora.io>
2021-12-21 09:15:55 +0000

    Merge pull request #7 in ADUC/open-apaas-extapp-ios from dev/HeZhengQing to release/apaas/2.0.0
    Squashed commit of the following:
    
    commit 685439bd29894d6f35cc012ec688fbc81f32a946
    Author: HeZhengQing <hezhengqing@agora.io>
    Date:   Tue Dec 21 16:51:50 2021 +0800
    
        [FEAT] RTM IM Widget

---
+++ Widgets/AgoraWidgets/RtmIM/Views/AgoraRtmIMReceiveCell.swift
@@ -0,0 +1,127 @@
+//
+//  AgoraRtmIMReceiveCell.swift
+//  AgoraWidgets
+//
+//  Created by Jonathan on 2021/12/17.
+//
+
+import UIKit
+
+fileprivate let kRoleLabelHeight: CGFloat = 16
+fileprivate let kAvatarHeight: CGFloat = 22
+class AgoraRtmIMReceiveCell: UITableViewCell {
+    
+    var messageModel: AgoraRtmMessageModel? {
+        didSet {
+            if messageModel != oldValue {
+                updateView()
+            }
+        }
+    }
+
+    private var avatarView: UIImageView!
+    
+    private var bubleView: UIView!
+    
+    private var nameLabel: UILabel!
+    
+    private var roleLabel: UILabel!
+    
+    private var messageLabel: UILabel!
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        createViews()
+        createConstrains()
+    }
+    
+    public func setMessage(msg: String) {
+        messageLabel.text = msg
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func updateView() {
+        guard let model = messageModel  else {
+            return
+        }
+        nameLabel.text = model.name
+        messageLabel.text = model.text
+        if let str = model.roleName,
+           str.count > 0 {
+            roleLabel.isHidden = false
+            roleLabel.text = "  \(str)  "
+        } else {
+            roleLabel.isHidden = true
+        }
+    }
+}
+// MARK: - Creations
+private extension AgoraRtmIMReceiveCell {
+    func createViews() {
+        avatarView = UIImageView()
+        avatarView.image = UIImage.ag_imageNamed("ic_rtm_avatar",
+                                                 in: "AgoraWidgets")
+        avatarView.contentMode = .scaleAspectFill
+        avatarView.layer.cornerRadius = kAvatarHeight * 0.5
+        contentView.addSubview(avatarView)
+        
+        nameLabel = UILabel()
+        nameLabel.textColor = UIColor(hex: 0x191919)
+        nameLabel.font = UIFont.systemFont(ofSize: 13)
+        contentView.addSubview(nameLabel)
+        
+        roleLabel = UILabel()
+        roleLabel.textColor = UIColor(hex: 0x586376)
+        roleLabel.font = UIFont.systemFont(ofSize: 12)
+        roleLabel.layer.borderWidth = 1
+        roleLabel.layer.cornerRadius = kRoleLabelHeight * 0.5
+        roleLabel.layer.borderColor = UIColor(hex: 0xABB1BA, transparency: 0.3)?.cgColor
+        contentView.addSubview(roleLabel)
+        
+        bubleView = UIView()
+        bubleView.backgroundColor = .white
+        bubleView.layer.borderWidth = 1
+        bubleView.layer.borderColor = UIColor(hex: 0xECECF1)?.cgColor
+        bubleView.layer.cornerRadius = 4
+        contentView.addSubview(bubleView)
+        
+        messageLabel = UILabel()
+        messageLabel.textColor = UIColor(hex: 0x191919)
+        messageLabel.numberOfLines = 0
+        messageLabel.font = UIFont.systemFont(ofSize: 13)
+        contentView.addSubview(messageLabel)
+    }
+    
+    func createConstrains() {
+        avatarView.mas_makeConstraints { make in
+            make?.top.equalTo()(5)
+            make?.left.equalTo()(14)
+            make?.width.height().equalTo()(kAvatarHeight)
+        }
+        nameLabel.mas_makeConstraints { make in
+            make?.centerY.equalTo()(avatarView)
+            make?.left.equalTo()(avatarView.mas_right)?.offset()(6)
+        }
+        roleLabel.mas_makeConstraints { make in
+            make?.height.equalTo()(kRoleLabelHeight)
+            make?.left.equalTo()(nameLabel.mas_right)?.offset()(6)
+            make?.centerY.equalTo()(nameLabel)
+        }
+        messageLabel.mas_makeConstraints { make in
+            make?.left.equalTo()(25)
+            make?.top.equalTo()(avatarView.mas_bottom)?.offset()(17)
+            make?.right.lessThanOrEqualTo()(-25)
+            make?.bottom.equalTo()(self.contentView)?.offset()(-15)
+        }
+        bubleView.mas_makeConstraints { make in
+            make?.left.equalTo()(messageLabel)?.offset()(-10)
+            make?.right.equalTo()(messageLabel)?.offset()(10)
+            make?.top.equalTo()(messageLabel)?.offset()(-9)
+            make?.bottom.equalTo()(messageLabel)?.offset()(9)
+        }
+    }
+}
---
+++ Widgets/AgoraWidgets/RtmIM/Views/AgoraRtmIMTopBar.swift
@@ -0,0 +1,47 @@
+//
+//  AgoraRtmIMTopBar.swift
+//  AgoraWidgets
+//
+//  Created by Jonathan on 2021/12/16.
+//
+
+import UIKit
+
+class AgoraRtmIMTopBar: UIView {
+    
+    private var titleLabel: UILabel!
+    
+    private var lineView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        
+        titleLabel = UILabel()
+        titleLabel.textColor = UIColor(rgb: 0x191919)
+        titleLabel.text = "rtm_title_chat".ag_localizedIn("AgoraWidgets")
+        titleLabel.font = UIFont.systemFont(ofSize: 13)
+        addSubview(titleLabel)
+        titleLabel.mas_makeConstraints { make in
+            make?.centerY.equalTo()(0)
+            make?.left.equalTo()(16)
+            make?.right.greaterThanOrEqualTo()(-16)
+        }
+        
+        lineView = UIView()
+        lineView.backgroundColor = UIColor(rgb: 0xECECF1)
+        addSubview(lineView)
+        lineView.mas_makeConstraints { make in
+            make?.left.right().bottom().equalTo()(0)
+            make?.height.equalTo()(1)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+    }
+}
