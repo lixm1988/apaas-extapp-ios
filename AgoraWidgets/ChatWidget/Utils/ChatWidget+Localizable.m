0607a3670c888cd237a6c3aa33ed39cf396da8ce
suruoxi <suruoxi@agora.io>
2022-01-17 06:51:09 +0000

    Merge pull request #36 in ADUC/open-apaas-extapp-ios from release/apaas/chat to release/apaas/2.1.0
    Squashed commit of the following:
    
    commit 8ec439ffcee9e3eec88c73d08aabfdd9b5afb315
    Author: Lyy <liuyuanyuan02@agora.io>
    Date:   Mon Jan 17 14:37:59 2022 +0800
    
        [ENH]:update for ChatWidget-AgoraWidgets

---
+++ AgoraWidgets/ChatWidget/Utils/ChatWidget+Localizable.m
@@ -0,0 +1,16 @@
+//
+//  ChatWidget+Localizable.m
+//  ChatWidget
+//
+//  Created by lixiaoming on 2021/7/13.
+//
+
+//
+//
+//@implementation ChatWidget (Localizable)
+//+ (NSString*)LocalizedString:(NSString*)aString
+//{
+//    NSBundle* bundle = [NSBundle bundleForClass:[ChatWidget class]];
+//    return NSLocalizedStringFromTableInBundle(aString, nil, bundle, nil);
+//}
+//@end
